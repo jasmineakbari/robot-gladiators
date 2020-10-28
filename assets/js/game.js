153337cb0b31bc5e293bbfd485a9983702c7d725
unknown <jasmineakbari@yahoo.com>
2020-10-28 19:30:45 -0400

    added for loop and added enemy names array

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,11 +4,11 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -31,7 +31,7 @@ var fight = function() {
         //remove player's health by subtracting the amount set in the enemyAttack variable
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyName + " attacked" + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
         );
 
         if (playerHealth <= 0) {
@@ -62,4 +62,12 @@ var fight = function() {
     }
 };
 
-fight();
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//    * Fight all enemy-robots
+//    * Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
