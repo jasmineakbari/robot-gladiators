5778796cbb84525a23a0fb5bfeb383a91a4ab274
unknown <jasmineakbari@yahoo.com>
2020-10-28 23:26:40 -0400

    added next round notification and player death text

--- assets/js/game.js
+++ assets/js/game.js
@@ -72,7 +72,14 @@ var fight = function(enemyName) {
 // "LOSE" - Player robot's health is zero or less
 
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + ( i + 1) );
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
+
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
-    fight(enemyNames[i]);
+    fight(pickedEnemyName);
 }
